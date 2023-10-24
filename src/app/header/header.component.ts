e25b69a81c78d95178f914833a2f0b91210c8e18
Cerdan <fillipe.cerdan@gmail.com>
2023-10-24 03:23:54 -0300

    Testes de funcionamento do sideNav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import * as M from 'materialize-css';
+
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit{
+  @ViewChild('mobile') sideNav?: ElementRef;
 
+  ngAfterViewInit(): void {
+    M.Sidenav.init(this.sideNav?.nativeElement, {edge:'right'});
+  }
 }
