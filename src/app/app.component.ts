e85d787d1ded4d7b5477271dd2b1cb22f1925d14
Cerdan <fillipe.cerdan@gmail.com>
2023-10-28 16:45:55 -0300

    Inclusão da modificação do título da página por Interpolação.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bug-tracker-fmwk';
+export class AppComponent implements OnInit{
+  title : string = 'Coa.Tracker';
+
+  constructor(private titleService : Title){
+    this.titleService.setTitle(this.title);
+  }
+
+  ngOnInit(): void {}
 }
