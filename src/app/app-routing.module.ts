2750ce4511fc2d26b479d112bb175a86e2261bfa
Cerdan <fillipe.cerdan@gmail.com>
2023-11-02 20:05:10 -0300

    Adição de rotas e correção do componente principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TicketsComponent } from './tickets/tickets.component';
+
+const routes: Routes = [
+  {path: 'index', component: LandingComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'ticket', component: TicketsComponent},
+  {path: '', redirectTo: 'index', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
