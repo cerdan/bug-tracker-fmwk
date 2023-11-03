b10853e399dce0bc2fc4ae1cb63a72ae319e3a10
Cerdan <fillipe.cerdan@gmail.com>
2023-11-03 20:42:32 -0300

    Adicionado o preenchimento do username através do @ViewChild + QueryParam

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,58 @@
-import { Component, EventEmitter, Output, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { NgxMaskDirective } from 'ngx-mask/lib/ngx-mask.directive';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  OnInit,
+} from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements AfterViewInit {
+export class LoginComponent implements OnInit, AfterViewInit {
   @Output() loginEvent = new EventEmitter<string>();
-  @ViewChild('usernameField') userField? : ElementRef;
-  username : string = '';
-  password : string = '';
+  @ViewChild('usernameField') userField?: ElementRef;
+  @ViewChild('passwordField') pwdField?: ElementRef;
+  username: string = '';
+  password: string = '';
+  userInput!: HTMLInputElement;
+  pwdInput!: HTMLInputElement;
 
-  constructor(){
+  emailPattern: any = { Ç: { pattern: new RegExp('[0-9A-Za-z.+]') } };
+  usernamePattern: any = { U: { pattern: new RegExp('[0-9A-Za-z.]') } };
+  namePattern: any = { N: { pattern: new RegExp('[A-Za-z ]') } };
 
+  constructor(private route: ActivatedRoute) {}
+  ngAfterViewInit(): void {
+    this.userInput = this.userField?.nativeElement as HTMLInputElement;
+    this.pwdInput = this.pwdField?.nativeElement as HTMLInputElement;
+
+    this.userInput.focus();
+    if (!this.username) return;
+
+    // https://github.com/JsDaddy/ngx-mask/issues/723 workaroud with setTimeout
+    setTimeout(() => {
+      this.userInput.value = this.username;
+      this.pwdInput.focus();
+    }, 1);
   }
 
-  ngAfterViewInit() : void{
-
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['username']) {
+        this.username = params['username'];
+      }
+    });
   }
 
-  onClickLogin() : void{
+  onClickLogin(): void {
     this.username = this.userField?.nativeElement.value;
-    this.loginEvent.emit(this.username);  
+    this.loginEvent.emit(this.username);
   }
 }
