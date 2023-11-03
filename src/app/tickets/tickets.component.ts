29843efc9994c71eef9b9a3c72b16f87278a8922
Cerdan <fillipe.cerdan@gmail.com>
2023-11-03 19:52:23 -0300

    Teste da tela de ticket com leitura de QueryParams

--- src/app/tickets/tickets.component.ts
+++ src/app/tickets/tickets.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.component.html',
   styleUrls: ['./tickets.component.css']
 })
-export class TicketsComponent {
+export class TicketsComponent implements OnInit{
+  username : string = '';
 
+  constructor(private route : ActivatedRoute) {
+    
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['username']) {
+        this.username = params['username'];
+      }
+    });
+  }
+
+  
 }
